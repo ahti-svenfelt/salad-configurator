b21e61d1c895d16fb07e4ba0dab790c465f79e81
ahti-svenfelt <svenfeltahti@gmail.com>
2026-03-26 15:19:15 +0200

    task 7

--- koodit/src/App.tsx
+++ koodit/src/App.tsx
@@ -1,10 +1,17 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
-
   return (
-    <>
+    <div className="min-h-screen flex flex-col bg-white font-sans">
+      <Header />
 
-    </>
+      <main className="flex-1 max-w-6x1 w-full mx-auto p-6 flex flex-col gap-8 mt-4">
+
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
--- koodit/src/components/Footer.tsx
+++ koodit/src/components/Footer.tsx
@@ -1,13 +1,15 @@
 export default function Footer() {
-    <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
-        <div>
-            Contact
-        </div>
-        <div>
-            Social Media
-        </div>
-        <div>
-            Branding
-        </div>
-    </footer>
+    return (
+        <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+            <div>
+                Contact
+            </div>
+            <div>
+                Social Media
+            </div>
+            <div>
+                Branding
+            </div>
+        </footer>
+    )
 }
