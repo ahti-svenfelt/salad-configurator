ab48b1a549e89adc1b5f11984299bd5626b007b9
e2402568 <e2402568@student.vamk.fi>
2026-03-27 11:32:54 +0200

    Task 13

--- koodit/src/App.tsx
+++ koodit/src/App.tsx
@@ -1,5 +1,10 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import BowlSelection from "./components/BowlSelection"
+import CenterBowl from "./components/CenterBowl"
+import BaseSelection from "./components/BaseSelection"
+import IngredientSection from "./components/IngredientSection"
+import SummaryBar from "./components/SummaryBar"
 
 function App() {
   return (
@@ -7,7 +12,14 @@ function App() {
       <Header />
 
       <main className="flex-1 max-w-6x1 w-full mx-auto p-6 flex flex-col gap-8 mt-4">
+        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
+          <BowlSelection />
+          <CenterBowl />
+          <BaseSelection />
+        </div>
+        <IngredientSection />
 
+        <SummaryBar />
       </main>
 
       <Footer />
