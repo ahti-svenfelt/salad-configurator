bf15708f2f6ebfac2cc7377e3bf868c623fdc2e6
e2402568 <e2402568@student.vamk.fi>
2026-03-27 10:28:09 +0200

    task 8

--- koodit/src/App.tsx
+++ koodit/src/App.tsx
@@ -1,14 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import BowlSelection from "./components/BowlSelection"
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white font-sans">
       <Header />
 
-      <main className="flex-1 max-w-6x1 w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-
-      </main>
+      <BowlSelection />
 
       <Footer />
     </div>
--- koodit/src/components/Header.tsx
+++ koodit/src/components/Header.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return(
         <header className="bg-zinc-800 text-white w-full h-32 flex justify-between items-start px-8 pt-4">
-            <Link to="/" className="w-24 h-24 rounded full border-4 border-[#A2D135] flex items-center justify-center flex-col -mt-2 bg-zinc-800 shadow-lg">
+            <a className="w-24 h-24 rounded full border-4 border-[#A2D135] flex items-center justify-center flex-col -mt-2 bg-zinc-800 shadow-lg">
                 Fresh Food Factory
                 FRESSE
-            </Link>
+            </a>
 
             <h1 className="text-3x1 font-black tracking-widest mt-6">
                 BOWL-LASKURI
