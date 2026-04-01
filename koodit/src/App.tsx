022b818df68822e555b8feff3e83514491065b44
ahti <svenfeltahti@gmail.com>
2026-04-01 13:17:10 +0300

    task 2.3

--- koodit/src/App.tsx
+++ koodit/src/App.tsx
@@ -5,6 +5,9 @@ import BaseSelection from "./components/BaseSelection"
 import IngredientSection from "./components/IngredientSection"
 import SummaryBar from "./components/SummaryBar"
 import Configurator from "./pages/Configurator"
+import { Routes, Route } from "react-router-dom"
+import Community from "./pages/Community"
+import Print from "./pages/Print"
 
 function App() {
   return (
@@ -12,16 +15,22 @@ function App() {
       <Header />
 
       <main className="flex-1 max-w-6x1 w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
-          <BowlSelection />
+        
+          <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
+            <BowlSelection />
+            <Routes>
+              <Route path="./pages/Configurator.tsx" element={<Configurator />} />
+              <Route path="./page/Community.tsx" element={<Community />} />
+              <Route path="./pages/Print.tsx" element={<Print />}/>
+            </Routes>
+            <BaseSelection />
+          </div>
+          <IngredientSection />
 
-          <Configurator />
+          <SummaryBar />
+
           
-          <BaseSelection />
-        </div>
-        <IngredientSection />
-
-        <SummaryBar />
+        
       </main>
 
       <Footer />
--- koodit/src/components/Header.tsx
+++ koodit/src/components/Header.tsx
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return(
         <header className="bg-zinc-800 text-white w-full h-32 flex justify-between items-start px-8 pt-4">
-            <a className="w-24 h-24 rounded full border-4 border-[#A2D135] flex items-center justify-center flex-col -mt-2 bg-zinc-800 shadow-lg">
+            <Link to="/" className="w-24 h-24 rounded full border-4 border-[#A2D135] flex items-center justify-center flex-col -mt-2 bg-zinc-800 shadow-lg">
                 Fresh Food Factory
                 FRESSE
-            </a>
+            </Link>
 
             <h1 className="text-3x1 font-black tracking-widest mt-6">
                 BOWL-LASKURI
             </h1>
 
             <div className="bg-[#A2D135] text-black rounded-b-3x1 rounded-t-xl px-6 py-4 flex flex-col gap-2 min-w-[200px] shadow-md">
-
+                <Link to="/community" className="font-semibold hover:underline">
+                    Saved recipes
+                </Link>
             </div>
         </header>
     )
