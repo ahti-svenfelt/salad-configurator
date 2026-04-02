c3076fec79d8c74945d489ab29df5f0db136aa46
e2402568 <e2402568@student.vamk.fi>
2026-04-02 11:26:09 +0300

    BowlSelection korjattu

--- koodit/src/components/BowlSelection.tsx
+++ koodit/src/components/BowlSelection.tsx
@@ -1,10 +1,14 @@
 import type { Bowl } from "../types";
+import { useIngredientStore } from "../store/useIngredientStore";
 
 interface BowlSelectionProps {
     bowls: Bowl[];
 }
 
 export default function BowlSelection({ bowls }: BowlSelectionProps) {
+    const setBowl = useIngredientStore((s) => s.setBowl);
+    const selectedBowl = useIngredientStore((s) => s.selectedBowl);
+
     return (
         <div className="bg-zinc-800 rounded-[3rem] p-6 text-white lg:w-1/4 flex flex-col items-center shadow-lg">
             <div className="bg-white text-black font-bold rounded-full w-8 h-8 flex items-center justify-center mb-4 shrink-0">
@@ -24,26 +28,13 @@ export default function BowlSelection({ bowls }: BowlSelectionProps) {
                     {bowl.name}
                 </button>
             ))}
-            
-            {bowls.map((bowl) => (
-                <button
-                    key={bowl.id}
-                    className={`h-12 border-2 rounded-xl flex items-center px-4 ${
-                        selectedBowl?.id === bowl.id
-                            ? "border-white"
-                            : "border-gray-600"
-                    }`}
-                    onClick={() => setBowl(bowl)}
-                >
-                    {bowl.name}
-                </button>
-            ))}
 
             <div className="h-12 border-2 border-gray-600 rounded-xl flex items-center px-4">
 
             {bowls.length === 0 && (
                 <p className="text-sm text-gray-400 mt-2">Ei kulhoja ladattu...</p>
             )}
+            </div>
         </div>
     );
 }
