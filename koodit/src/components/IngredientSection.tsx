65f075e243303e56dd3198813462f8d9c5802bf2
ahti <svenfeltahti@gmail.com>
2026-04-08 13:44:48 +0300

    task 3.4

--- koodit/src/components/IngredientSection.tsx
+++ koodit/src/components/IngredientSection.tsx
@@ -1,13 +1,19 @@
 import IngredientCard from "./IngredientCard"
 import type { Category, Ingredient } from "../types"
+import { useState } from "react";
 
 interface IngredientSectionProps {
     categories: Category[];
     ingredients: Ingredient[];
 }
 export default function IngredientSection({ categories, ingredients }: IngredientSectionProps) {
+    const [activeCategory, setActiveCategory] = useState<"all" | number>("all")
+
     const filteredCategories = categories.filter(cat => cat.id !== 6)
-    const filteredIngredients = ingredients.filter(ing => ing.categoryId !== 6)
+    const filteredIngredients = 
+        activeCategory === "all"
+            ? ingredients.filter(ing => ing.categoryId !== 6)
+            : ingredients.filter(ing => ing.categoryId === activeCategory)
 
     return(
         <div className="bg-zinc-800 rounded-[3rem] p-8 text-white w-full shadow-lg">
@@ -15,11 +21,16 @@ export default function IngredientSection({ categories, ingredients }: Ingredien
                 <input type="text" className="rounded-full px-6 py-3 text-black outline-none w-64 border-2 border-transparent focus:border-[#A2D135]"/>
             </div>
             <div>
-                {filteredCategories.map(cat => (
-                    <p key={cat.id} className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full">
-                        {cat.name}
-                    </p>
-                ))}
+                <button
+                    onClick={() => setActiveCategory("all")}
+                    className={`px-6 py-2 rounded-full font-bold ${
+                        activeCategory === "all"
+                            ? "bg-[#A2D135] text-black"
+                            : "bf-zinc-600 text-white"
+                    }`}
+                >
+                    All
+                </button>
                 {filteredCategories.map(cat => (
                     <p key={cat.id} className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full">
                         {cat.name}
