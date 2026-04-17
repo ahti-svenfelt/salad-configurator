77a846108776dd45e5a9ada5130bd4064ad90e05
e2402568 <e2402568@student.vamk.fi>
2026-04-17 10:10:30 +0300

    task 5.2

--- koodit/src/services/api.ts
+++ koodit/src/services/api.ts
@@ -1,25 +1,45 @@
 const Base_url = "https://fresse-api.onrender.com/api";
 
-export async function getBowls() {
-    const response = await fetch(`${Base_url}/bowls?type_id=[1|2]`)
-    if (!response.ok) {
-        throw new Error("Failed to fetch");
+export async function login(email: string, password: string) {
+    const response = await fetch(`${Base_url}/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+    });
+
+    if(!response.ok) {
+        throw new Error("Invalid credentials");
     }
+
     return response.json();
 }
 
-export async function getCategories() {
-    const response = await fetch(`${Base_url}/categories?type_id=[|2]`)
+export async function getBowls(id: string) {
+    const response = await fetch(`${Base_url}/bowls?type_id=${id}`);
+
     if (!response.ok) {
         throw new Error("Failed to fetch");
     }
+
+    return response.json();
+}
+
+export async function getCategories(id: string) {
+    const response = await fetch(`${Base_url}/categories?type_id=${id}`);
+    
+    if (!response.ok) {
+        throw new Error("Failed to fetch");
+    }
+
     return response.json();
 }
 
 export async function getIngredients() {
-    const response = await fetch(`${Base_url}/baseingredients`)
+    const response = await fetch(`${Base_url}/baseingredients`);
+
     if (!response.ok) {
         throw new Error("Failed to fetch");
     }
+
     return response.json();
 }
